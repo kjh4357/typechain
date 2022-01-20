e7be066ff702ecaf55eda808aafdabae2e89f278
kjh4357 <kjh4357@gmail.com>
2022-01-21 02:00:53 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import * as CryptoJS from "crypto-js";
+
+class Block {
+    public index:number;
+    public hash:string;
+    public previousHash:string;
+    public data: string;
+    public timestamp: number;
+
+    static calculateBlockHash = (index:number, previousHash:string, timestamp:number, data:string) : string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+    constructor(
+        index:number,
+        hash:string,
+        previousHash:string,
+        data: string,
+        timestamp: number,
+    ){
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
+}
+
+
+const genesisBlock:Block = new Block(0, "2020202020", "", "Hello", 123456);
+
+let blockchain: Block[] = [genesisBlock];
+
+const getBlockchain = () : Block[] => blockchain;
+
+const getLastestBlock = () : Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = () : number => Math.round(new Date().getTime() / 1000);
+ 
+console.log(blockchain)
+export {};
